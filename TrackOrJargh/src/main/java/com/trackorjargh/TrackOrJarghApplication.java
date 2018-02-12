d0472af198c3c3c098dc2caa5c4cbc00f673400e
krate95 <jesus200495@gmail.com>
2018-02-12 12:05:02 +0100

    Created Eclipse STS project

---
+++ TrackOrJargh/src/main/java/com/trackorjargh/TrackOrJarghApplication.java
@@ -0,0 +1,12 @@
+package com.trackorjargh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackOrJarghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackOrJarghApplication.class, args);
+	}
+}
