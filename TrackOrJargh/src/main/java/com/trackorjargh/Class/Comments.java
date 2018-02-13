bb4ddf2a83cbf1d514ba4356af632b159d2f6c61
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-02-13 21:43:11 +0100

    add dependencys bbdd in pom.xml

--- TrackOrJargh/src/main/java/com/trackorjargh/Class/Comments.java
+++ TrackOrJargh/src/main/java/com/trackorjargh/Class/Comments.java
@@ -1,6 +1,16 @@
 package com.trackorjargh.Class;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Comments {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
 	public User user;
 	public String comment;
 	public float points;
