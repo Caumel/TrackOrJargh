f084aa4c629dbec7dfb885b5061399c34ea0c4d9
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-02-19 19:18:44 +0100

    repair crash error user logged

--- TrackOrJargh/src/main/java/com/trackorjargh/configurers/UserHandlerConfigurer.java
+++ TrackOrJargh/src/main/java/com/trackorjargh/configurers/UserHandlerConfigurer.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,28 +15,28 @@ import com.trackorjargh.component.UserComponent;
 
 @Configuration
 public class UserHandlerConfigurer extends WebMvcConfigurerAdapter {
-	@Autowired
-	private UserComponent userComponent;
-	
+	@Bean
+	public UserHandlerInterceptor userHandlerInterceptor() {
+		return new UserHandlerInterceptor();
+	}
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new UserHandlerInterceptor(userComponent));
+		registry.addInterceptor(userHandlerInterceptor());
 	}
 }
 
 class UserHandlerInterceptor extends HandlerInterceptorAdapter {
+	@Autowired
 	private UserComponent userComponent;
-	
-	public UserHandlerInterceptor(UserComponent userComponent) {
-		this.userComponent = userComponent;
-	}
-	
+
 	@Override
 	public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler,
 			final ModelAndView modelAndView) throws Exception {
-			
-			if(userComponent.isLoggedUser()) {
+		if (modelAndView != null) {
+			if (userComponent.isLoggedUser()) {
 				modelAndView.addObject("userLogged", userComponent.getLoggedUser());
-			}		
+			}
+		}
 	}
 }
