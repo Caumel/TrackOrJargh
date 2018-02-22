b61b933bf5cd8e415b023bca259da9b9cf714382
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-02-22 18:31:09 +0100

    added messages in search

--- TrackOrJargh/src/main/java/com/trackorjargh/javacontrollers/SearchController.java
+++ TrackOrJargh/src/main/java/com/trackorjargh/javacontrollers/SearchController.java
@@ -1,12 +1,16 @@
 package com.trackorjargh.javacontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.trackorjargh.javaclass.Book;
+import com.trackorjargh.javaclass.Film;
+import com.trackorjargh.javaclass.Show;
 import com.trackorjargh.javarepository.BookRepository;
 import com.trackorjargh.javarepository.FilmRepository;
 import com.trackorjargh.javarepository.ShowRepository;
@@ -22,36 +26,56 @@ public class SearchController {
 	private ShowRepository showRepository;
 
 	@RequestMapping("/busqueda")
-	public String search(Model model) {		
+	public String search(Model model) {	
+		model.addAttribute("noElementsSearch", true);
+		model.addAttribute("index", true);
 		return "search";
 	}
 	
 	@RequestMapping("/busqueda/{optionSearch}/pelicula/{name}")
-	public String searchFilms(Model model, @PathVariable String optionSearch, @PathVariable String name) {		
-		model.addAttribute("content", filmRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10)));
+	public String searchFilms(Model model, @PathVariable String optionSearch, @PathVariable String name) {
+		Page<Film> films = filmRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10));
+		
+		model.addAttribute("content", films);
 		model.addAttribute("typeFilm", true);
 		model.addAttribute("inputSearch", name);
 		model.addAttribute("typeSearch", "/rest/busqueda/" + optionSearch +"/peliculas/" + name + "/page");
+				
+		if(films.getNumberOfElements() == 0) {
+			model.addAttribute("noElementsSearch", true);
+		}
 		
 		return "search";
 	}
 	
 	@RequestMapping("/busqueda/{optionSearch}/serie/{name}")
 	public String searchShows(Model model, @PathVariable String optionSearch, @PathVariable String name) {		
-		model.addAttribute("content", showRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10)));
+		Page<Show> shows = showRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10));
+		
+		model.addAttribute("content", shows);
 		model.addAttribute("typeShow", true);
 		model.addAttribute("inputSearch", name);
 		model.addAttribute("typeSearch", "/rest/busqueda/" + optionSearch +"/series/" + name + "/page");
+		
+		if(shows.getNumberOfElements() == 0) {
+			model.addAttribute("noElementsSearch", true);
+		}
 		
 		return "search";
 	}
 	
 	@RequestMapping("/busqueda/{optionSearch}/libro/{name}")
 	public String searchBooks(Model model, @PathVariable String optionSearch, @PathVariable String name) {		
-		model.addAttribute("content", bookRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10)));		
+		Page<Book> books = bookRepository.findByNameContainingIgnoreCase(name, new PageRequest(0, 10));
+		
+		model.addAttribute("content", books);		
 		model.addAttribute("typeBook", true);
 		model.addAttribute("inputSearch", name);
 		model.addAttribute("typeSearch", "/rest/busqueda/" + optionSearch +"/libros/" + name + "/page");
+		
+		if(books.getNumberOfElements() == 0) {
+			model.addAttribute("noElementsSearch", true);
+		}
 		
 		return "search";
 	}
