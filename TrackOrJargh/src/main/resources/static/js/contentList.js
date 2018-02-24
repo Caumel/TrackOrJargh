f382f840e27cf1f9c5f23253019b3b82f5aa7f21
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-02-24 19:56:35 +0100

    minor error item.sypnosis and added lists

--- TrackOrJargh/src/main/resources/static/js/contentList.js
+++ TrackOrJargh/src/main/resources/static/js/contentList.js
@@ -7,6 +7,7 @@ function loadContent() {
 	if(request < totalPages){
 		$("#moreContent").attr("class","fa fa-spinner fa-pulse fa-3x fa-fw");
 		$("#moreContent").unbind("click");
+		$("[name='buttonList']").unbind("click");
 		
 		$.get(typePage +"?page=" + request + "&size=" + numElements, function(data) {
 			sleep(1000);
@@ -34,11 +35,11 @@ function loadContent() {
 						'<div class="col-lg-3 col-md-3 portfolio-item filter-app wow fadeInUp">' +
 							'<div class="portfolio-wrap">' +
 								'<figure style="background-image: url(' + item.image + ');">' +
-									'<p class="textPortfolio">item.synopsis</p>' +
+									'<p class="textPortfolio">' + item.synopsis + '</p>' +
 								'</figure>' +
 								'<div class="portfolio-info">' +
 									'<h4>' + 
-										'<a href="item.url + item.name +">' + item.name + '</a>' +
+										'<a name="nameFilm" href="item.url + item.name +">' + item.name + '</a>' +
 									'</h4>' + 
 									'<p>' + item.year + '</p>' +
 									addedLists + 
@@ -49,6 +50,7 @@ function loadContent() {
 			
 			$("#moreContent").attr("class","fa fa-plus-circle fa-3x fa-fw");
 			$("#moreContent").click(loadContent);
+			$("[name='buttonList']").click(addFilmToList);
 			request++;
 			
 			if(request == totalPages){
