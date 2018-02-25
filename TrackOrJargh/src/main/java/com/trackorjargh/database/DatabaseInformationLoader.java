89daab038e9f89be52ef40d8b543a89153ccfb19
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-02-25 20:14:29 +0100

    minnor changes

--- TrackOrJargh/src/main/java/com/trackorjargh/database/DatabaseInformationLoader.java
+++ TrackOrJargh/src/main/java/com/trackorjargh/database/DatabaseInformationLoader.java
@@ -61,7 +61,7 @@ public class DatabaseInformationLoader {
 	@PostConstruct
 	private void initDatabase() {
 		// Test Data User
-		User u1 = new User("oscar", "1234", "oscarsotosanchez@gmail.com", "/img/default-user.png", true, "ROLE_USER");
+		User u1 = new User("oscar", "1234", "oscarmola@gmail.com", "/img/default-user.png", true, "ROLE_USER");
 		userRepository.save(u1);
 		User u2 = new User("jesus", "1234", "jesusmola@gmail.com", "/img/userFoto.jpg", true, "ROLE_USER",
 				"ROLE_MODERATOR", "ROLE_ADMIN");
