98001d5cd8965c4ea4deb78bf62da7aea49594c5
Óscar Soto Sánchez <OscarSotoSanchez@users.noreply.github.com>
2018-03-10 13:03:08 +0100

    added configuration docker

---
+++ Docker/create_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+path_project=$(dirname $(pwd))/TrackOrJargh
+path_jar=$path_project/target
+
+#Create jar TrackOrjargh
+docker run -it --rm --name trackorjargh -v "$path_project":/usr/src/mymaven -w /usr/src/mymaven maven mvn package -DskipTests
+
+#Move jar to actual directory
+mv $path_jar/TrackOrJargh-0.0.1-SNAPSHOT.jar .
+
+#Create image 
+docker build -t trackorjargh-spring .
